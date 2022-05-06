4d37d5dbff5aa1fb94f3165783e3ad176a821829
Cyber-Kira <kirillkoloskov03@gmail.com>
2022-05-06 18:59:05 +0300

    feat: add random quote"

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,39 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useAppDispatch, useAppSelector } from './app/hooks'
+import { getRandomQuote } from './features/quote/quoteSlice'
 
 const App = () => {
+	const { content, isLoading, author, genre } = useAppSelector(
+		store => store.quote
+	)
+	const dispatch = useAppDispatch()
+
+	useEffect(() => {
+		dispatch(getRandomQuote())
+	}, [])
+
+	const quoteSkeleton = <div>Loading...</div>
+
+	const randomQuote = (
+		<>
+			<div>Quote: {content}</div>
+			<div>Author: {author}</div>
+			<div>Genre: {genre}</div>
+		</>
+	)
+
 	return (
-		<div className='App'>
-			<header className='App-header'>
-				<div>Test</div>
+		<div className='container md:mx-auto md:max-w-3xl'>
+			<header>
+				<button
+					onClick={() => dispatch(getRandomQuote())}
+					type='button'
+					className='bg-cyan-300 py-1 px-2 rounded-lg m-12'
+				>
+					Random Quote
+				</button>
 			</header>
+			<main>{isLoading ? quoteSkeleton : randomQuote}</main>
 		</div>
 	)
 }

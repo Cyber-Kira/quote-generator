f951c2479f047c047406a8f4669414f82e74d5e4
Cyber-Kira <kirillkoloskov03@gmail.com>
2022-05-07 23:22:53 +0300

    feat: add author card component

---
+++ src/components/AuthorCard/AuthorCard.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { AuthorCardSkeleton } from '../../lib/components/AuthorCardSkeleton'
+
+interface Props {
+	isLoading: boolean
+	author: string
+	genre: string
+}
+
+export const AuthorCard = ({ isLoading, author, genre }: Props) => {
+	const authorElement = (
+		<article className='w-full max-w-author'>
+			<div className='flex items-center px-8 py-12 hover:bg-[#333333] group transition-colors'>
+				<div>
+					<p className='text-main font-raleway text-2xl leading-7 font-bold group-hover:text-[#F2F2F2]'>
+						{author}
+					</p>
+					<span className='text-[#828282] mt-2 font-raleway font-medium text-sm leading-4'>
+						{genre}
+					</span>
+				</div>
+				<span className='material-symbols-outlined pl-4 invisible text-[#F2F2F2] ml-auto group-hover:visible'>
+					arrow_right_alt
+				</span>
+			</div>
+		</article>
+	)
+
+	return isLoading ? <AuthorCardSkeleton /> : authorElement
+}

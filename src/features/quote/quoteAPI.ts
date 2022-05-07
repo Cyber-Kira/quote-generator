c157b1622eee7ccb1d72ab3db8463e71edc9696e
Cyber-Kira <kirillkoloskov03@gmail.com>
2022-05-07 23:06:52 +0300

    feat: add fetch function to fetch quotes

--- src/features/quote/quoteAPI.ts
+++ src/features/quote/quoteAPI.ts
@@ -26,3 +26,27 @@ export const fetchRandomQuote = () => {
 		})
 		.catch(err => err)
 }
+
+export const fetchQuotes = ({
+	author = '',
+	genre = '',
+	query = '',
+	page = 1,
+	limit = 20,
+}: QuoteInterface) => {
+	const params = new URLSearchParams({
+		author,
+		genre,
+		query,
+		page: String(page),
+		limit: String(limit),
+	})
+	return fetch(`${url}?${params}`)
+		.then(response => {
+			if (!response.ok) {
+				throw new Error(response.statusText)
+			}
+			return response.json()
+		})
+		.catch(err => err)
+}

1068a369e9d8df9d25e2a08120fc6004af4237bd
Cyber-Kira <kirillkoloskov03@gmail.com>
2022-05-07 23:24:06 +0300

    feat: add custom properties

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,22 @@
 module.exports = {
 	content: ['./src/**/*.{ts,tsx}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				main: '#4F4F4F',
+			},
+			fontFamily: {
+				raleway: ['Raleway', 'sans-serif'],
+			},
+			height: {
+				header: '75px',
+				withHeader: 'calc(100vh - 75px)',
+			},
+			maxWidth: {
+				quote: '721px',
+				author: '672px',
+			},
+		},
 	},
 	plugins: [],
 }

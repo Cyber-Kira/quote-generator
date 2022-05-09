d476cbb29d6ff4774628fb7bb96ceeb9fc82401a
Cyber-Kira <kirillkoloskov03@gmail.com>
2022-05-09 18:27:25 +0300

    feat: add router links

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,18 +1,33 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import { useAppDispatch, useAppSelector } from '../../app/hooks'
-import { getRandomQuote } from '../../features/quote/quoteSlice'
+import { getRandomQuote } from '../../features/quote/randomQuoteSlice'
 
 export const Header = () => {
-	const { isLoading } = useAppSelector(store => store.quote)
+	const { isLoading } = useAppSelector(store => store.randomQuote)
 	const dispatch = useAppDispatch()
 
 	return (
 		<header className='h-header'>
 			<nav className='flex justify-end items-center gap-1 w-full h-full'>
+				<Link
+					className='px-3 py-2 flex items-center font-raleway font-medium text-lg leading-5 text-main hover:text-purple-600 transition-colors'
+					to='/'
+				>
+					home
+				</Link>
+				<Link
+					className='px-3 py-2 flex items-center font-raleway font-medium text-lg leading-5 text-main hover:text-purple-600 transition-colors'
+					to='/search'
+				>
+					search
+				</Link>
 				<button
-					onClick={() => dispatch(getRandomQuote())}
-					className='px-3 py-2 flex items-center gap-1 font-raleway font-medium text-lg leading-5 text-main'
 					type='button'
+					onClick={() => {
+						dispatch(getRandomQuote())
+					}}
+					className='px-3 py-2 flex items-center gap-1 font-raleway font-medium text-lg leading-5 text-main hover:text-purple-600 transition-colors'
 				>
 					random
 					<span

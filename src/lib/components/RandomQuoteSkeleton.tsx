407a0924386b1aeb0a966d2ff99a1273e40583ce
Cyber-Kira <kirillkoloskov03@gmail.com>
2022-05-09 18:32:54 +0300

    Feat: rename components

--- src/lib/components/RandomQuoteSkeleton.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-export const RandomQuoteSkeleton = () => (
-	<div className='flex gap-8 md:gap-24 max-w-quote'>
-		<div className='h-44 w-2 bg-slate-300 animate-pulse' />
-		<div className='w-full bg-slate-300 animate-pulse rounded' />
-	</div>
-)

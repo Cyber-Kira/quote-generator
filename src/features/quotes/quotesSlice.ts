b5b323467d2b7862420b7c0c98287208393d3c9c
Cyber-Kira <kirillkoloskov03@gmail.com>
2022-05-09 18:32:21 +0300

    refactoring: change names

---
+++ src/features/quotes/quotesSlice.ts
@@ -0,0 +1,45 @@
+/* eslint-disable no-underscore-dangle */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit'
+import {
+	fetchQuotes,
+	QuotesInterface,
+	RandomQuoteInterface,
+} from '../quote/quoteAPI'
+
+const initialState: QuotesInterface = {
+	data: [],
+	pagination: { currentPage: 0, nextPage: 1, totalPages: 2 },
+	totalQuotes: 0,
+	isLoading: false,
+}
+
+export const getQuotes = createAsyncThunk(
+	'counter/getQuotes',
+	async ({ author, genre, query, page, limit }: RandomQuoteInterface) => {
+		const response = await fetchQuotes({ author, genre, query, page, limit })
+		// The value we return becomes the `fulfilled` action payload
+		return response
+	}
+)
+
+export const quotesSlice = createSlice({
+	name: 'quotes',
+	initialState,
+	reducers: {},
+	extraReducers: builder => {
+		builder
+			.addCase(getQuotes.pending, state => {
+				state.isLoading = true
+			})
+			.addCase(getQuotes.fulfilled, (state, { payload }) => {
+				state.data = payload.data
+				state.pagination = payload.pagination
+				state.totalQuotes = payload.totalQuotes
+				state.isLoading = false
+			})
+			.addCase(getQuotes.rejected, state => {
+				state.isLoading = false
+			})
+	},
+})

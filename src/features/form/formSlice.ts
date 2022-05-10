98cc62de72685090419ba3c9da345d2ebcd281b0
Cyber-Kira <kirillkoloskov03@gmail.com>
2022-05-10 19:43:14 +0300

    feat: add form slice to keep track of input values

---
+++ src/features/form/formSlice.ts
@@ -0,0 +1,33 @@
+/* eslint-disable no-underscore-dangle */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { createSlice } from '@reduxjs/toolkit'
+
+interface FormInterface {
+	author?: string
+	genre?: string
+	query?: string
+}
+
+const initialState: FormInterface = {
+	author: '',
+	genre: '',
+	query: '',
+}
+
+export const formSlice = createSlice({
+	name: 'quotes',
+	initialState,
+	reducers: {
+		setAuthor: (state, { payload }) => {
+			state.author = payload
+		},
+		setGenre: (state, { payload }) => {
+			state.genre = payload
+		},
+		setQuery: (state, { payload }) => {
+			state.query = payload
+		},
+	},
+})
+
+export const { setAuthor, setGenre, setQuery } = formSlice.actions

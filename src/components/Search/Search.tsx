aea497b16c4b052d82ef56ed73a6f406982d762f
Cyber-Kira <kirillkoloskov03@gmail.com>
2022-05-11 17:32:06 +0300

    feat: add form and quotes list to search component

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,44 +1,68 @@
 /* eslint-disable no-underscore-dangle */
-import React from 'react'
-import { Button, TextField } from '@mui/material'
+import React, { useEffect } from 'react'
+import { Button, Pagination } from '@mui/material'
 import { useAppSelector, useAppDispatch } from '../../app/hooks'
 import { getQuotes } from '../../features/quotes/quotesSlice'
-import { Quote } from '../Quote/Quote'
 import { Authors } from './components/Authors'
 import { Genres } from './components/Genres'
+import { Query } from './components/Query'
+import { Quotes } from './components/Quotes'
 
 export const Search = () => {
-	const { data, isLoading } = useAppSelector(store => store.quotes)
+	const { pagination } = useAppSelector(store => store.quotes)
 	const { genre, author, query } = useAppSelector(store => store.form)
 	const dispatch = useAppDispatch()
 
-	const handleChange = () => {
-		dispatch(getQuotes({ author, genre, query, limit: Number(20) }))
+	const handleChange = (value?: number) => {
+		if (value) {
+			dispatch(
+				getQuotes({ author, genre, query, page: value, limit: Number(20) })
+			)
+		} else {
+			dispatch(getQuotes({ author, genre, query, limit: Number(20) }))
+		}
 	}
 
+	useEffect(() => {
+		dispatch(getQuotes({ author: '', genre: '', query: '', limit: Number(20) }))
+		const listener = (event: KeyboardEvent) => {
+			if (event.code === 'Enter' || event.code === 'NumpadEnter') {
+				event.preventDefault()
+				dispatch(getQuotes({ author, genre, query, limit: Number(20) }))
+			}
+		}
+		document.addEventListener('keydown', listener)
+		return () => {
+			document.removeEventListener('keydown', listener)
+		}
+	}, [])
+
 	return (
-		<div className='flex flex-col gap-4 max-w-quote mx-auto w-full mt-5 bg-slate-100'>
-			<form className='flex flex-col gap-4 w-full'>
-				<TextField id='query-string' label='Query' defaultValue='' />
+		<div className='flex flex-col max-w-quote mx-auto w-full mt-5'>
+			<form className='flex flex-col md:flex-row gap-4 w-full'>
+				<Query />
 				<Authors />
 				<Genres />
 				<Button
+					className='flex-shrink-0'
 					onClick={() => handleChange()}
+					disableElevation
 					variant='contained'
 					endIcon={<span className='material-symbols-outlined'>search</span>}
 				>
 					Search
 				</Button>
 			</form>
-			<div className='flex flex-col gap-32 my-8'>
-				{data.map(item => (
-					<Quote
-						content={item.quoteText}
-						isLoading={isLoading}
-						key={item._id}
-					/>
-				))}
+			<div className='flex justify-center mt-2'>
+				<Pagination
+					className='my-3'
+					color='primary'
+					count={pagination.totalPages}
+					page={pagination.currentPage}
+					onChange={(_event, value) => handleChange(value)}
+				/>
 			</div>
+			<Quotes />
 		</div>
 	)
 }

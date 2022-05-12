5bc6ccd2a778dbbe01b42b65be0e78b463f5bbbd
Cyber-Kira <kirillkoloskov03@gmail.com>
2022-05-12 15:58:54 +0300

    feat: add footer component

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export const Footer = () => {
+	return (
+		<div className='font-montserrat text-sm leading-4 text-footer font-semibold text-center py-9 mt-10'>
+			created by
+			<a
+				href='https://devchallenges.io/portfolio/Cyber-Kira'
+				target='_blank'
+				rel='noreferrer'
+				className='font-montserrat text-sm text-footer leading-4 font-bold px-1'
+			>
+				CyberKira
+			</a>
+			- devChallenges.io
+		</div>
+	)
+}

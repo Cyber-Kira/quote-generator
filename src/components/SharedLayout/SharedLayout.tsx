1f110508be376c34d82b600f23fd678299b8f2c6
Cyber-Kira <kirillkoloskov03@gmail.com>
2022-05-12 15:58:38 +0300

    feat: add footer:

--- src/components/SharedLayout/SharedLayout.tsx
+++ src/components/SharedLayout/SharedLayout.tsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import { Footer } from '../Footer/Footer'
 import { Header } from '../Header/Header'
 
 export const SharedLayout = () => {
 	return (
-		<div className='container px-6 mx-auto md:max-w-7xl md:px-0 h-full'>
+		<div className='container px-6 mx-auto md:max-w-7xl md:px-0'>
 			<Header />
-			<main className='flex h-withHeader'>
+			<main>
 				<Outlet />
 			</main>
+			<footer>
+				<Footer />
+			</footer>
 		</div>
 	)
 }

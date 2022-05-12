da4b7cc55fa3447da33e782235bc88f62b10aa3f
Cyber-Kira <kirillkoloskov03@gmail.com>
2022-05-12 15:59:23 +0300

    feat: update custom values and fonts for footer component

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,13 +4,15 @@ module.exports = {
 		extend: {
 			colors: {
 				main: '#4F4F4F',
+				footer: '#828282',
 			},
 			fontFamily: {
 				raleway: ['Raleway', 'sans-serif'],
+				montserrat: ['Montserrat', 'sans-serif'],
 			},
 			height: {
 				header: '75px',
-				withHeader: 'calc(100vh - 75px)',
+				withHeader: 'calc(100vh - 211px)',
 			},
 			maxWidth: {
 				quote: '721px',
